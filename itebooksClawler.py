059a0ecde5b2194a45d2bd94194e06ea9cd0419b
linguanghuan <linguanghuan@163.com>
2017-05-07 00:18:09 +0800

    save file

--- itebooksClawler.py
+++ itebooksClawler.py
@@ -3,26 +3,134 @@
 # http://www.allitebooks.com/
 # http://www.allitebooks.com/page/1/ - http://www.allitebooks.com/page/709/
 # 一页有10本, 总共 7006本 一本10M算  需要70G的空间
+
 import traceback
 import requests
 from bs4 import BeautifulSoup
+import threadpool
+import sys
+import os
+import datetime
 
-if __name__ == "__main__":
-    print "get books"
-    pageUrls = ["http://www.allitebooks.com/page/{}/".format(str(i))for i in range(1,800)]
-    pageUrls = ["http://www.allitebooks.com/page/{}/".format(str(i))for i in range(1,2)]
-    print pageUrls
-    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+linkPool = []
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
+basePath = "F:/allitebooks"
+os.chdir(basePath)
+
+def get_a_book(link):
     try:
-        resp = requests.get(pageUrls.pop(), headers=headers)
-        resp.encoding="utf-8"
+        print "=======================begin to get book", link
+        dirname = link.split("/")[3]
+        print "dirname", dirname
+        if not os.path.isdir(dirname):
+            try:
+                os.mkdir(dirname)
+            except:
+                traceback.print_exc()
+                return
+            
+        resp = requests.get(link, headers=headers)
         soup = BeautifulSoup(resp.text, "lxml")
-        entry_titles = soup.findAll("h2",{"class":"entry-title"})
-        for entry_title in entry_titles:
-#             print entry_title
-            link = entry_title.a.get('href')
-            title = entry_title.a.string
-            print link
-            print title
+        try:
+            detail = soup.find("div",{"class":"book-detail"}).get_text()
+            print "detail", detail
+            desc = soup.find("div",{"class":"entry-content"}).get_text()
+            print "desc", desc
+            
+        except:
+            traceback.print_exc()
+        try:
+            downloadLink =  soup.find("span",{"class":"download-links"}).a.get('href')
+            print "download", downloadLink
+            filename = downloadLink.split("/")[-1]
+        except:
+            traceback.print_exc()
+            return
+        
+        fullFileName = dirname+"/"+filename
+        if os.path.exists(fullFileName):
+            print "file already exist:",  fullFileName
+            return
+        try:
+            descFile = dirname + "/" + dirname + ".txt"
+            file = open(descFile,"w")
+            file.write(detail)
+            file.write(desc)
+        except:
+            traceback.print_exc()
+        finally:
+            file.close()
+            
+        content = requests.get(downloadLink, headers = headers)
+        try:
+            file = open(fullFileName,"wb")
+            file.write(content._content)
+            file.close()
+        except:
+            traceback.print_exc()
+        finally:
+            file.close()
     except:
         traceback.print_exc()
+
+def test():
+    print range(708, -1, -1)
+    return
+    str = "http://www.allitebooks.com/build-mobile-apps-with-ionic-2-and-firebase/"
+    print str.split("/")[3]
+    str2 = "http://file.allitebooks.com/20170505/Build Mobile Apps with Ionic 2 and Firebase.pdf"
+    print str2.split("/")[-1]
+
+    
+if __name__ == "__main__":
+#     test()
+#     sys.exit()
+    print "allitebooks clawler"
+    now = datetime.datetime.now()
+    today = now.strftime('%Y-%m-%d')
+    recordFile = "all_files_"+today+".txt" 
+    if os.path.exists(recordFile):
+        file = open(recordFile, "r")
+        for line in file:
+            linkPool.append(line)
+    else:
+        pageUrls = ["http://www.allitebooks.com/page/{}/".format(str(i+1))for i in range(708, -1, -1)]
+    #     pageUrls = ["http://www.allitebooks.com/page/{}/".format(str(i))for i in range(1,2)]
+        try:
+            while len(pageUrls) > 0:
+                pageUrl = pageUrls.pop()
+                print "deal", pageUrl
+                resp = requests.get(pageUrl, headers=headers)
+                resp.encoding="utf-8"
+                soup = BeautifulSoup(resp.text, "lxml")
+                entry_titles = soup.findAll("h2",{"class":"entry-title"})
+                for entry_title in entry_titles:
+        #             print entry_title
+                    link = entry_title.a.get('href')
+                    title = entry_title.a.string
+                    print "get", link
+        #             print title
+                    linkPool.append(link)
+            file = open(recordFile, "w")
+            try:                
+                for link in linkPool:
+                    file.write(link + '\n')
+            except:
+                traceback.print_exc()
+            finally:
+                file.close()
+        except:
+            traceback.print_exc()
+        finally:
+            pass
+    
+    pool = threadpool.ThreadPool(40)
+    threadRequests = threadpool.makeRequests(get_a_book, linkPool)
+    [pool.putRequest(req) for req in threadRequests]
+    pool.wait()
+    print "finishied"
+        
+        
