73f9769f4c240470676a420e5d00947da1b1527c
test <test@home>
2017-06-13 21:56:29 +0800

    mul thread down load meta info

--- itebooksClawler.py
+++ itebooksClawler.py
@@ -3,6 +3,11 @@
 # http://www.allitebooks.com/
 # http://www.allitebooks.com/page/1/ - http://www.allitebooks.com/page/709/
 # 一页有10本, 总共 7006本 一本10M算  需要70G的空间
+#pip install requests
+#pip install bs4
+#pip install lxml
+#pip install bs4
+#pip install threadpool
 
 import traceback
 import requests
@@ -11,31 +16,66 @@ import threadpool
 import sys
 import os
 import datetime
+import threading
+import shutil
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-linkPool = []
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
-basePath = "F:/allitebooks"
-os.chdir(basePath)
+g_lock = threading.Lock()
 
-def get_a_book(link):
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
+
+now = datetime.datetime.now()
+today = now.strftime('%Y-%m-%d')
+recordFile = "all_files_"+today+".txt"
+newPath = "E:/book2/"
+oldPath = "E:/allitebooks"
+manualPath = "E:/allitebooks_manual"
+os.chdir(newPath)
+recordfd = open(recordFile, "a") 
+
+pageUrls = ["http://www.allitebooks.com/page/{}/".format(str(i+1))for i in range(715, -1, -1)]
+exist = {}
+linkPool = []
+
+def loadExists():
+    os.chdir(oldPath)
+    subdirs = os.listdir(os.getcwd());
+    for subdir in subdirs:
+        exist[subdir] = "1"
+
+def checkExist(key):
+    try:
+        if exist[key]=="1":
+            return True
+        return False
+    except:
+        return False
+
+def mov(src, dst):
+    shutil.move(src, dst)
+        
+def getPdfBook(link):
     try:
         print "=======================begin to get book", link
         dirname = link.split("/")[3]
         print "dirname", dirname
+        if checkExist(dirname)==True:
+            print "skip exist job", dirname
+            return
+        
         if not os.path.isdir(dirname):
             try:
                 os.mkdir(dirname)
             except:
                 traceback.print_exc()
                 return
-            
-        if os.path.exists(os.path.join(dirname, "success_mark.txt")):
+        success_mark = os.path.join(dirname, "success_mark.txt")   
+        if os.path.exists(success_mark):
             print "skip success job:", dirname
             return
-          
+
         resp = requests.get(link, headers=headers)
         soup = BeautifulSoup(resp.text, "lxml")
         try:
@@ -55,8 +95,13 @@ def get_a_book(link):
             return
         
         fullFileName = dirname+"/"+filename
+        manualFile =  os.path.join(manualPath, filename)
         if os.path.exists(fullFileName):
             print "file already exist:",  fullFileName
+            return
+        elif os.path.exists(manualFile):
+            print "move from manual path"
+            mov(manualFile, fullFileName)
             return
         try:
             descFile = dirname + "/" + dirname + ".txt"
@@ -89,14 +134,40 @@ def test():
     print str2.split("/")[-1]
 
     
+def getBooks(pageUrl):  
+    try:
+        print "deal", pageUrl
+        resp = requests.get(pageUrl, headers=headers, timeout=30)
+        resp.encoding="utf-8"
+        soup = BeautifulSoup(resp.text, "lxml")
+        entry_titles = soup.findAll("h2",{"class":"entry-title"})
+        for entry_title in entry_titles:
+#             print entry_title
+            link = entry_title.a.get('href')
+            title = entry_title.a.string
+            print "get", link
+#             print title
+            linkPool.append(link)
+            g_lock.acquire()
+            recordfd.write(link + '\n')
+            g_lock.release()
+        recordfd.flush()
+        return True
+    except:
+        traceback.print_exc() 
+        pageUrls.append(pageUrl)
+        return False
+                 
 if __name__ == "__main__":
 #     test()
+    loadExists()
+    if checkExist("adding-ajax")==True:
+        print "111111111"
 #     sys.exit()
     print "allitebooks clawler"
-    now = datetime.datetime.now()
-    today = now.strftime('%Y-%m-%d')
-    recordFile = "all_files_"+today+".txt" 
-    if os.path.exists(recordFile):
+    
+    os.chdir("E:/book2/")
+    if os.path.exists(recordFile) and os.path.getsize(recordFile) > 10:
         try:
             file = open(recordFile, "r")
             for line in file:
@@ -105,37 +176,21 @@ if __name__ == "__main__":
             traceback.print_exc()
             sys.exit()
     else:
-        pageUrls = ["http://www.allitebooks.com/page/{}/".format(str(i+1))for i in range(299, -1, -1)]
-    #     pageUrls = ["http://www.allitebooks.com/page/{}/".format(str(i))for i in range(1,2)]
-
         try:
             file = open(recordFile, "w")
-            while len(pageUrls) > 0:
-                try:
-                    pageUrl = pageUrls.pop()
-                    print "deal", pageUrl
-                    resp = requests.get(pageUrl, headers=headers)
-                    resp.encoding="utf-8"
-                    soup = BeautifulSoup(resp.text, "lxml")
-                    entry_titles = soup.findAll("h2",{"class":"entry-title"})
-                    for entry_title in entry_titles:
-            #             print entry_title
-                        link = entry_title.a.get('href')
-                        title = entry_title.a.string
-                        print "get", link
-            #             print title
-                        linkPool.append(link)
-                        file.write(link + '\n')
-                    file.flush()
-                except:
-                    traceback.print_exc()
+            pool = threadpool.ThreadPool(40)
+            threadRequests = threadpool.makeRequests(getBooks, pageUrls)
+            [pool.putRequest(req) for req in threadRequests]
+            pool.wait()
         except:
             traceback.print_exc()
         finally:
             file.close()
-                
+#     sys.exit()
+
+    os.chdir(newPath)            
     pool = threadpool.ThreadPool(40)
-    threadRequests = threadpool.makeRequests(get_a_book, linkPool)
+    threadRequests = threadpool.makeRequests(getPdfBook, linkPool)
     [pool.putRequest(req) for req in threadRequests]
     pool.wait()
     print "finishied"
